61469d18187ce5dfc7b06aaab5ec6603f6e16678
sunsunny-hub <surajsingh5233@gmail.com>
2022-10-18 19:02:01 +0530

    s3 bucket region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tf-state-bucket-remote"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "tfstate-dynamodb-table"
   }
 }
