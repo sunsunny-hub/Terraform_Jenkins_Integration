27329362de13388981b89dd571c28da8a377e3fb
sunsunny-hub <surajsingh5233@gmail.com>
2022-10-18 16:44:02 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "tf-state-bucket-remote"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "tfstate-dynamodb-table"
   }
 }
